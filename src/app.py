6e7ed45b74ae643ddecd86ef2e536a688332cd9a
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2022-09-23 16:14:48 +0900

    streamlit.md 一旦完成

--- src/app.py
+++ src/app.py
@@ -1,6 +1,12 @@
 import streamlit as st
+from PIL import Image
 
-st.title("サンプルアプリ")
+st.title("Streamlit入門")
 
-st.write("私の名前は{YOUR_NAME}です。")
+image_file = st.file_uploader("ファイルを選択してください。", type=["png", "jpg"])
+
+if image_file is not None:
+    image_byte_data = image_file.read()
+    st.image(image_byte_data, width=500)  # byte型のデータも画像として表示できる。
+
 
