0d87830f6462bea8d701987e84196931b89f75c4
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2022-11-26 14:30:17 +0900

    アプリの完成

--- src/app.py
+++ src/app.py
@@ -1,35 +1,30 @@
 import streamlit as st
+import io
+import os
 
-st.title("ああああ")
+from google.cloud import vision
 
-## タイトル
-st.write("こんにちは!!私の名前は{YOUR_NAME}です。")
-
-## 文字入力
-text = st.text_input("sample")
-if text:
-    st.write(f"入力された文字 : {text}")
-
-st.write("-"*40)  # ハイフンを40個出す。
-
-## テキストボックス(ボタン押下で反映)
-text2 = st.text_input("sample2")
-if st.button("出力"):
-    st.balloons()
-    st.write(f"入力された文字 : {text2}")
-
-st.write("-"*40)  # ハイフンを40個出す。
-
-## 画像の表示
-st.image("src/images/pose_pien_uruuru_woman.png", width=500, caption="🥺")
-
-st.write("-"*40)  # ハイフンを40個出す。
+# タイトル
+st.title("文字検出アプリ")
 
 ## 画像ファイルのアップロード。
 image_file = st.file_uploader("画像ファイルをアップロードしてください。", type=['png', 'jpg'])
 if image_file is not None:
     image_file_byte = image_file.read()
+    st.write(type(image_file_byte))
+    image = vision.Image(content=image_file_byte)
     st.image(image_file_byte, width=500)
+
+    # クライアントを定義
+    client = vision.ImageAnnotatorClient()
+    # ラベル抽出を適用
+    response = client.text_detection(image=image)
+    annotations = response.text_annotations
+
+    st.write("検出された文字")
+    for annotation in annotations:
+        st.write(annotation.description)
+
 
 
 
